2a7e54fe7b687585106e7fc6394931a70e0a48ff
jmiyazaki22 <jmiyazaki@netprotections.co.jp>
2018-09-18 16:25:08 +0900

    パスワードのパラメーターを変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,7 +58,7 @@ class UsersController < ApplicationController
   end
 
   def login
-    @user = User.find_by(email: params[:email], params[:password])
+    @user = User.find_by(email: params[:email], password: params[:password])
     if @user
       session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
